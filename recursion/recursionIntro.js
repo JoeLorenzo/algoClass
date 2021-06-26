9ad10af1b7f822166ab13e5c46173007f657cbb1
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2021-06-25 22:18:20 -0500

    -finished first recursive example

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -1,6 +1,31 @@
 //1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
 
+function countDownLoop(number) {
+  for (number; number >= 0; number--) {
+    console.log(`loop count down: `, number);
+  }
+}
+const loopTest = countDownLoop(5);
+
+function countDownWhileLoop(number) {
+  while (number >= 0) {
+    console.log(`While loop count down: `, number);
+    number--;
+  }
+}
+
+const whileLoopTest = countDownWhileLoop(5);
 //2. Next, try looping just like above except using recursion
+function countDownRecursive(number) {
+  if (number === 0) {
+    return console.log(`recursive count down: `, number);
+  } else {
+    console.log(`recursive count down: `, number);
+    return countDownRecursive(number - 1);
+  }
+}
+
+const testRecursion = countDownRecursive(5);
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
 
